9148789ef0a47d50961164f3389e119d05d61922
abhinav-adtechs <abhinav.adtechs@gmail.com>
2016-01-07 12:27:13 +0530

    Fragments in

---
+++ app/src/main/java/app/hackdev/jordiie/ffcshelper/Fragments/SlotsAvailable.java
@@ -0,0 +1,26 @@
+package app.hackdev.jordiie.ffcshelper.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.hackdev.jordiie.ffcshelper.R;
+
+/**
+ * Created by jordiie on 7/1/16.
+ */
+public class SlotsAvailable extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_slots_available, container, false) ;
+    }
+}
--- app/src/main/java/app/hackdev/jordiie/ffcshelper/MainActivity.java
+++ app/src/main/java/app/hackdev/jordiie/ffcshelper/MainActivity.java
@@ -1,15 +1,33 @@
 package app.hackdev.jordiie.ffcshelper;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.internal.MDTintHelper;
+import com.afollestad.materialdialogs.internal.ThemeSingleton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText passwordInput;
+    private View positiveAction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +40,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showCustomView();
             }
         });
     }
@@ -49,4 +66,51 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void showCustomView() {
+        MaterialDialog dialog = new MaterialDialog.Builder(this)
+                .title(R.string.choosecourse)
+                .customView(R.layout.dialog_course, true)
+                .positiveText(R.string.setcourse)
+                .negativeText(android.R.string.cancel)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                    }
+                }).build();
+
+        positiveAction = dialog.getActionButton(DialogAction.POSITIVE);
+        //noinspection ConstantConditions
+        /*passwordInput = (EditText) dialog.getCustomView().findViewById(R.id.password);
+        passwordInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                positiveAction.setEnabled(s.toString().trim().length() > 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        */
+        // Toggling the show password CheckBox will mask or unmask the password input EditText
+        CheckBox checkbox = (CheckBox) dialog.getCustomView().findViewById(R.id.addlab);
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                passwordInput.setInputType(!isChecked ? InputType.TYPE_TEXT_VARIATION_PASSWORD : InputType.TYPE_CLASS_TEXT);
+                passwordInput.setTransformationMethod(!isChecked ? PasswordTransformationMethod.getInstance() : null);
+            }
+        });
+
+        dialog.show();
+        positiveAction.setEnabled(false); // disabled by default
+    }
+
+
 }
